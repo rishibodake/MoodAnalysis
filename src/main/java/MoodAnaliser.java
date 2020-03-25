725a463ee9a73ec5b25d1db6557bc0283e86c214
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-25 14:01:27 +0530

    Refactor:message field added & added default constructor

--- src/main/java/MoodAnaliser.java
+++ src/main/java/MoodAnaliser.java
@@ -1,5 +1,13 @@
 public class MoodAnaliser {
     String message;
+
+    public static void main(String[] args) {
+        System.out.println("WelCome to mood Analiser");
+    }
+    //Default Constructor
+    public MoodAnaliser() {
+
+    }
     //Function that return mood
     public String analyseMood () {
         if (message.contains("Sad"))
