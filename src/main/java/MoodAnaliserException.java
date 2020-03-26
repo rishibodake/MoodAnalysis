48a1c2c77ec8478f6e2c965eed85a30a45567113
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-26 10:52:25 +0530

    MoodAnalyserFactory Created

---
+++ src/main/java/MoodAnaliserException.java
@@ -0,0 +1,17 @@
+public class MoodAnaliserException extends Exception{
+
+
+    enum UserDefinedDataType
+    {
+        NULL_EXCEPTION,
+        EMPTY_EXCEPTION,
+        NO_SUCH_CLASS,
+        NO_SUCH_METHOD
+    };
+    UserDefinedDataType userDefinedObject;
+    public MoodAnaliserException(String message, UserDefinedDataType userDefinedObject)
+    {
+        super(message);
+        this.userDefinedObject = userDefinedObject;
+    }
+}
