bc9e308d4af6a08360da8e4292675a5df59b9807
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-27 13:44:23 +0530

    Added:InvokeMethod Function in MoodAnalyserFactory

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -2,6 +2,8 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 //Default constructor
 public class MoodAnalyserFactory {
     public static Constructor<?> getConstructor(String className) throws MoodAnaliserException {
@@ -21,6 +23,21 @@ public class MoodAnalyserFactory {
             return (MoodAnaliser) constructor.newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static Object invokeMethod(Object moodAnalyserObject, String analyseMood) throws MoodAnaliserException{
+        try {
+            Class objectClass = moodAnalyserObject.getClass();
+            Method moodMethod = objectClass.getMethod(analyseMood);
+            Object result = moodMethod.invoke(moodAnalyserObject);
+            return result;
+        }catch (NoSuchMethodException e) {
+            throw new MoodAnaliserException("Method Not Found",MoodAnaliserException.UserDefinedDataType.NO_SUCH_METHOD);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
